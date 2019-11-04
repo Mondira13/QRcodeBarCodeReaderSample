a73605d4f125ab463f5620bc26c59afe1dd98393
Mondira Pal <mondira.p@encoders.co.in>
2019-11-04 16:44:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/qrcodesample/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.qrcodesample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button qrCode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.show_scan_details);
+        qrCode = findViewById(R.id.btn_scan);
+
+        qrCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+                integrator.setPrompt("Scanning ...");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents()==null){
+                Toast.makeText(this, "You canclled the Scanning", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                textView.setText(result.getContents());
+            }
+
+        }else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+
+}
